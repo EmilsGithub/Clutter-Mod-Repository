fbb6053d046cc1512283d28a0c88a21764f0657e
Emil <emilsvardgronvikacc@gmail.com>
2024-08-13 23:53:03 +0200

    1.20.(0-1)-0.5.10
    - Fixed version number.

--- src/main/java/net/emilsg/clutter/Clutter.java
+++ src/main/java/net/emilsg/clutter/Clutter.java
@@ -21,7 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Clutter implements ModInitializer {
-	public static final String MOD_VERSION = "0.5.9";
+	public static final String MOD_VERSION = "0.5.10";
 	public static final String MOD_ID = "clutter";
 	public static final Logger LOGGER = LoggerFactory.getLogger("Clutter");
 	public static final boolean IS_TRINKETS_LOADED = FabricLoader.getInstance().getModContainer("trinkets").isPresent();
